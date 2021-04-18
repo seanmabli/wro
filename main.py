f8bd8cad7153e29e55ce717819d4b864720c6609
Sean-Mabli <smdrone1@gmail.com>
2021-04-18 13:56:12 -0400

    '2021-04-18'

--- main.py
+++ main.py
@@ -1,20 +1,42 @@
 #!/usr/bin/env pybricks-micropython
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
-                                 InfraredSensor, UltrasonicSensor, GyroSensor)
-from pybricks.parameters import Port, Stop, Direction, Button, Color
-from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.ev3devices import Motor, ColorSensor
+from pybricks.parameters import Port
+from pybricks.tools import wait
 from pybricks.robotics import DriveBase
-from pybricks.media.ev3dev import SoundFile, ImageFile
+import time
 
+Ed = EV3Brick()
 
-# This program requires LEGO EV3 MicroPython v2.0 or higher.
-# Click "Open user guide" on the EV3 extension tab for more information.
+left_motor = Motor(Port.B)
+right_motor = Motor(Port.C)
+left_color = ColorSensor(Port.S2)
+right_color = ColorSensor(Port.S3)
 
+robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=120)
+robot.settings(straight_speed=400, turn_rate=65)
 
-# Create your objects here.
-ev3 = EV3Brick()
+BLACK = 9
+WHITE = 70
+threshold = (BLACK + WHITE) / 2
 
+PROPORTIONAL_GAIN = 4
 
-# Write your program here.
-ev3.speaker.beep()
+while True:
+    deviation = ((left_color.reflection() - threshold) + (right_color.reflection() - threshold)) / 2
+    turn_rate = PROPORTIONAL_GAIN * deviation
+    robot.drive(100, turn_rate)
+
+'''
+robot.straight(600)
+robot.turn(-90)
+robot.straight(900)
+robot.straight(-700)
+robot.turn(90)
+robot.straight(1500)
+robot.straight(-2100)
+
+Ed.screen.clear()
+Ed.screen.draw_text(0, 0, (time.time() - StartTime))
+time.sleep(10)
+'''
