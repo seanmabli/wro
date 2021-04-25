77fb9563a640e591d2be2ccc27139df4bd1bceda
Sean-Mabli <smdrone1@gmail.com>
2021-04-25 15:52:34 -0400

    .

--- combos.py
+++ combos.py
@@ -3,7 +3,10 @@ import numpy as np
 Info = np.zeros((4, 3, 4))
 Info[0, :, :] = np.array([[0, 0, 1, 2], [1, 0, 1, 2], [2, 2, 0, 1]])
 
-def FindLocation(Input):
+def findLocation(Input):
   Order = np.zeros((3, 4))
-  while (np.sum(Order) != 6):
-    Info[0, 1:, :]
+  if(np.where(Info[0, 1:, 0] == Input) != []):
+    Order[int(str(np.where(Info[0, 1:, 1] == Input))[8:9]) + 1, 1] = 1
+  return Order
+
+print(findLocation(0))
