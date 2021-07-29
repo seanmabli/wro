a3b8672982bd2ba861f1a1ee896f98865638c134
Sean-Mabli <smdrone1@gmail.com>
2021-07-28 21:11:46 -0400

    remove numpy

--- combos.py
+++ combos.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 '''
 Input Color:
 Red = 0, Green = 1, Blue = 2
@@ -23,31 +21,30 @@ RED, GREEN, BLUE = 0, 1, 2
 
 class Location:
   def __init__(self):
-    self.BoardColor = np.array([[0, 0, 1, 2], [1, 0, 1, 2], [2, 2, 0, 1]])
-    self.LocOccupied = np.zeros((3, 4))
-    self.Order = np.zeros((3, 4))
+    self.BoardColor = [[0, 0, 1, 2], [1, 0, 1, 2], [2, 2, 0, 1]]
+    self.LocOccupied = [[0]*4 for _ in range(3)]
+    self.Order = [[0]*4 for _ in range(3)]
 
   def SetLocation(self, RunNum, InputColor):
     for k in range(3):
       for j in range(4):
         for i in range(2):
-          if(self.Order[i + RunNum, j] == 0 and self.BoardColor[i + RunNum, j] == InputColor[k]):
-            self.Order[i + RunNum, j] = 1
+          if(self.Order[i + RunNum][j] == 0 and self.BoardColor[i + RunNum][j] == InputColor[k]):
+            self.Order[i + RunNum][j] = 1
             break
         else:
           continue
         break
 
   def SetLocationAsOccupied(self, Coordinates):
-    self.LocOccupied[Coordinates[0], Coordinates[1]] = 1
+    self.LocOccupied[Coordinates[0]][Coordinates[1]] = 1
 
   def SetLocationAsDelivered(self, Coordinates):
-    self.LocOccupied[Coordinates[0], Coordinates[1]] = 1
-    self.Order[Coordinates[0], Coordinates[1]] = 0
+    self.LocOccupied[Coordinates[0]][Coordinates[1]] = 1
+    self.Order[Coordinates[0]][Coordinates[1]] = 0
 
 Loc = Location()
-
-Loc.SetLocation(1, np.array([BLUE, BLUE, BLUE]))
+Loc.SetLocation(1, [BLUE, BLUE, BLUE])
 
 print(Loc.Order)
 Loc.SetLocationAsDelivered([2, 0])
